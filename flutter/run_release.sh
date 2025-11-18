a99583f40d04806e31cedbd0264046607df4a983
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2025-11-18 11:16:44 +0900

    small fix flutter/

--- flutter/run_release.sh
+++ flutter/run_release.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# Flutter リリースモードで実行（デバッグモードより10-20倍速い）
+# Flutter リリースモードで実行（デバッグモードより速い）
+# 画像表示してるときとか特にdebugだと厳しい
+# テストだったら--profileでもいいかも
 
 cd robot_ui
 flutter run -d linux --release

6ca4bddb4051d1d407e1b31002e7db64f27049a5
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2025-11-25 13:34:20 +0900

    add servo testing script

---
+++ scripts/servo/launch.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Script for launching Dynamixel Wizard2 in wayland
+# Set an environmental variable to force it to launch by using X11
+
+
+
+QT_QPA_PLATFORM=xcb ~/ROBOTIS/DynamixelWizard2/DynamixelWizard2.sh
